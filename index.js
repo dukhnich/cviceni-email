1065a93269c3c1587ecadddd6e3180061bb88db8
Maryna Dukhnich <maryna.dukhnich@gmail.com>
2023-09-25 18:49:08 +0200

    add goodbye function

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('funguju');
+const goodbye = (name) => {
+    const elClosing = document.querySelector('.email__closing');
+    if (elClosing) {
+        elClosing.innerHTML = `Na shledanou <br> S pozdravem ${name}`;
+    }
+};
+goodbye('Pavel Ovesný');
