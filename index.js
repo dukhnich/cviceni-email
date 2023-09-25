53c0052ff488d26cf38a5a54a1f257af091f6083
Maryna Dukhnich <maryna.dukhnich@gmail.com>
2023-09-25 18:56:17 +0200

    add content's function

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const goodbye = (name) => {
+const goodbye = (name = '') => {
     const elClosing = document.querySelector('.email__closing');
-    if (elClosing) {
+    if (elClosing && name) {
         elClosing.innerHTML = `Na shledanou <br> S pozdravem ${name}`;
     }
 };
-goodbye('Pavel Ovesný');
+const fillSubject = (subject = '') => {
+    const elSubject = document.querySelector('.email__subject');
+    if (elSubject && subject) {
+        elSubject.textContent = subject;
+    }
+};
+const fillBody = (body = '', name = '') => {
+    const elBody = document.querySelector('.email__body');
+    if (elBody && body) {
+        elBody.textContent = body;
+    }
+    goodbye(name);
+};
+
+fillSubject('Korporatní novinky');
+fillBody('Nic nového.', 'Pavel Ovesný')
