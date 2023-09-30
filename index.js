4179736afe12be3f5d635a34389d7f3a9c9add9c
Maryna Dukhnich <maryna.dukhnich@gmail.com>
2023-09-30 10:45:18 +0200

    refactoring fillBody to higher order function

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const goodbye = (name = '') => {
-    const elClosing = document.querySelector('.email__closing');
-    if (elClosing && name) {
-        elClosing.innerHTML = `Na shledanou <br> S pozdravem ${name}`;
-    }
+    return `Na shledanou <br> ${name}`;
+};
+const formalGoodbye = (name = '') => {
+    return `S uctivou poklonou <br> ${name}`;
+};
+const rudeGoodbye = (name = '') => {
+    return `Se měj <br> ${name}`;
 };
 const fillSubject = (subject = '') => {
     const elSubject = document.querySelector('.email__subject');
@@ -10,13 +13,26 @@ const fillSubject = (subject = '') => {
         elSubject.textContent = subject;
     }
 };
-const fillBody = (body = '', name = '') => {
+const fillBody = (body = '', name = '', goodbyeFunction) => {
     const elBody = document.querySelector('.email__body');
     if (elBody && body) {
         elBody.textContent = body;
     }
-    goodbye(name);
+    if (typeof goodbyeFunction !== 'function') {
+        return;
+    }
+    const elClosing = document.querySelector('.email__closing');
+    if (elClosing && name) {
+        elClosing.innerHTML = goodbyeFunction(name);
+    }
 };
 
-fillSubject('Korporatní novinky');
-fillBody('Nic nového.', 'Pavel Ovesný')
+fillSubject('Obchodní sdělení');
+fillBody(
+  'Kupte mycí prostředek, který vám vytře zrak, se slevou 50 %.',
+  'Jan Čistý',
+  formalGoodbye
+);
+fillSubject('Pozvánka na oslavu narozenin');
+fillBody('Zítra oslava. 18:00 ve Starý hospodě.', 'Patrik Veselý', rudeGoodbye);
+
